8799d52a08a27f604408096544131e3250c9c273
DevelopingUnicorn <snimaa11@htlkaindorf.at>
2014-12-16 16:58:55 +0100

    Full MultiLang Support for MovieInformation Display

--- src/at/movielist/beans/Movie.java
+++ src/at/movielist/beans/Movie.java
@@ -1,11 +1,14 @@
 package at.movielist.beans;
 
 import java.io.Serializable;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Movie implements Serializable {
 
     private String name, width, height, aspectratio, duration, filesize, fileextension;
     private int numberoffiles;
+    private ResourceBundle resBundle = ResourceBundle.getBundle("at.movielist.src.ResourceBundle", Locale.ENGLISH);
 
     public Movie(String name, String width, String height, String aspectratio, String duration, String filesize, String fileextension, int numberoffiles) {
         this.name = name;
@@ -18,6 +21,10 @@ public class Movie implements Serializable {
         this.numberoffiles = numberoffiles;
     }
 
+    public void setResBundle(Locale loc) {
+        this.resBundle = ResourceBundle.getBundle("at.movielist.src.ResourceBundle", loc);
+    }
+    
     public String getName() {
         return name;
     }
@@ -53,23 +60,23 @@ public class Movie implements Serializable {
     public String toHTMLString() {
         StringBuilder sb = new StringBuilder();
         sb.append("<br><center><h1>").append(name).append("</h1><hr noshade width='80%' ><table width='100%' style='font-size:12px' >");
-        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>Duration:</strong></td><td>").append(duration).append("</td></tr>");
+        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>").append(resBundle.getString("main_information_duration")).append(":</strong></td><td>").append(duration).append("</td></tr>");
 
         if (width.equals("") && height.equals("")) {
-            sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>Resolution:</strong></td><td width='50%' >DVD</td></tr>");
+            sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>").append(resBundle.getString("main_information_resolution")).append(":</strong></td><td width='50%' >DVD</td></tr>");
         } else {
             if (Integer.parseInt(width) >= 1280) {
-                sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>Resolution:</strong></td><td width='50%' >").append(width).append("x").append(height).append("  HD</td></tr>");
+                sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>").append(resBundle.getString("main_information_resolution")).append(":</strong></td><td width='50%' >").append(width).append("x").append(height).append("  HD</td></tr>");
             } else {
-                sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>Resolution:</strong></td><td width='50%' >").append(width).append("x").append(height).append("  SD</td></tr>");
+                sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>").append(resBundle.getString("main_information_resolution")).append(":</strong></td><td width='50%' >").append(width).append("x").append(height).append("  SD</td></tr>");
             }
 
         }
 
-        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>Display Aspect Ratio:</strong></td><td width='50%' >").append(aspectratio).append("</td></tr>");
-        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>Fileextension:</strong></td><td width='50%' >").append(fileextension).append("</td></tr>");
-        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>Filesize:</strong></td><td width='50%' >").append(filesize).append("</td></tr>");
-        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>Number of Files:</strong></td><td width='50%' >").append(numberoffiles).append("</td></tr>");
+        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>").append(resBundle.getString("main_information_dar")).append(":</strong></td><td width='50%' >").append(aspectratio).append("</td></tr>");
+        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>").append(resBundle.getString("main_information_fileext")).append(":</strong></td><td width='50%' >").append(fileextension).append("</td></tr>");
+        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>").append(resBundle.getString("main_information_filesize")).append(":</strong></td><td width='50%' >").append(filesize).append("</td></tr>");
+        sb.append("<tr><td width='50%' ><strong style='color:#00bda5'>").append(resBundle.getString("main_information_numboffiles")).append(":</strong></td><td width='50%' >").append(numberoffiles).append("</td></tr>");
         sb.append("</table></center></body>");
 
         return sb.toString();
