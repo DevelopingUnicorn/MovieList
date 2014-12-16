4d8278ae80e7d5a168c2129d4637c43cdef5682d
DevelopingUnicorn <snimaa11@htlkaindorf.at>
2014-12-16 18:52:48 +0100

    Removed dll

--- src/at/lib/mediainfo/MediaInfoException.java
+++ src/at/lib/mediainfo/MediaInfoException.java
@@ -9,11 +9,13 @@ public class MediaInfoException extends RuntimeException {
 	
 	public MediaInfoException(LinkageError e) {
 		this(String.format("Unable to load %d-bit native library 'mediainfo'", Platform.is64Bit() ? 64 : 32), e);
+                e.printStackTrace();
 	}
 	
 
 	public MediaInfoException(String msg, Throwable e) {
 		super(msg, e);
+                e.printStackTrace();
 	}
 	
 }
