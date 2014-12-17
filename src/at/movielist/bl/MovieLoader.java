db66884eeda7a3ba07f8419fa961b33709e299ed
DevelopingUnicorn <snimaa11@htlkaindorf.at>
2014-12-17 13:21:10 +0100

    Better Perormance

--- src/at/movielist/bl/MovieLoader.java
+++ src/at/movielist/bl/MovieLoader.java
@@ -2,25 +2,23 @@ package at.movielist.bl;
 
 import at.movielist.ui.MainUI;
 import at.movielist.ui.ProgressbarDLG;
+import java.util.Locale;
 
 public class MovieLoader {
 
     private String pathtomov;
     private MovieLoaderWorker mlw;
     private ProgressbarDLG dlg;
+    private Locale loc;
 
-    public MovieLoader(String pathtomov, ProgressbarDLG pb) {
+    public MovieLoader(String pathtomov, ProgressbarDLG pb, Locale loc) {
         this.pathtomov = pathtomov;
         this.dlg = pb;
+        this.loc = loc;
     }
 
     public void getMovies(MainUI mui) {   
-        mlw = new MovieLoaderWorker(this.pathtomov, dlg, mui);
+        mlw = new MovieLoaderWorker(this.pathtomov, dlg, mui, loc);
         mlw.execute();
-    }
-    
-    public void getMoviesFromFile()
-    {
-        
     }
 }
--- src/at/movielist/bl/MovieLoaderWorker.java
+++ src/at/movielist/bl/MovieLoaderWorker.java
@@ -11,6 +11,8 @@ import javax.swing.SwingWorker;
 import at.lib.mediainfo.MediaInfo;
 import at.movielist.ui.MainUI;
 import at.movielist.ui.ProgressbarDLG;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class MovieLoaderWorker extends SwingWorker<LinkedList<Movie>, Movie> {
 
@@ -22,34 +24,44 @@ public class MovieLoaderWorker extends SwingWorker<LinkedList<Movie>, Movie> {
     private MainUI mui;
     private UtilityClass uc = new UtilityClass();
     private SimpleDateFormat durationFormat = new SimpleDateFormat("k'h' mm'mn'");
+    private ResourceBundle resBundle = ResourceBundle.getBundle("at.movielist.src.ResourceBundle", Locale.ENGLISH);
 
+    private String prog1, prog2;
+    
     private static final String[] okFileExtensions
             = new String[]{".mkv", ".avi", ".mp4", ".ogg", ".flv", ".3gp", ".iso", ".img", ".vob", ".ts", ".mpg", ".m2ts"};
     private static final String[] filesToIgnore
             = new String[]{"ds_store", ".nfo", ".mp3"};
 
-    public MovieLoaderWorker(String path, ProgressbarDLG d, MainUI mui) {
+    public MovieLoaderWorker(String path, ProgressbarDLG d, MainUI mui, Locale loc) {
         this.path = path;
         this.loading = d.getProgBar();
         this.lb = d.getLabel();
         this.dlg = d;
         this.mui = mui;
+        this.resBundle = ResourceBundle.getBundle("at.movielist.src.ResourceBundle", loc);
+        
+        prog1 = resBundle.getString("progress_string_1");
+        prog2 = resBundle.getString("progress_string_2");
     }
 
     public LinkedList<Movie> getListe() {
         return liste;
     }
-
+ 
     @Override
     protected LinkedList<Movie> doInBackground() throws Exception {
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
         MediaInfo mi = new MediaInfo();
 
+        int length = listOfFiles.length;
+        
         dlg.setMovieWorker(this);
 
-        for (int i = 0; i < listOfFiles.length; i++) {
-            String xfy = "Loading Movie " + (i + 1) + " from " + listOfFiles.length;
+        for (int i = 0; i < length; i++) {
+            StringBuilder sb = new StringBuilder();
+            String xfy = sb.append(prog1).append(" ").append((i + 1)).append(" ").append(prog2).append(" ").append(length).toString();
             double inc = 1000000 / listOfFiles.length;
             lb.setText(xfy);
             if (listOfFiles[i].isDirectory()) {
