319900f29b6f42bec002a407903f6c42f7d8c57d
DevelopingUnicorn <snimaa11@htlkaindorf.at>
2015-10-01 14:24:01 +0200

    Added AdvancedSearchDLG

---
+++ src/at/movielist/ui/AdvancedSearchDLG.java
@@ -0,0 +1,22 @@
+package at.movielist.ui;
+
+public class AdvancedSearchDLG extends javax.swing.JDialog {
+
+    public AdvancedSearchDLG(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        getContentPane().setLayout(new java.awt.GridLayout(1, 3));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
